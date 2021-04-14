b64e95f641817561a1e3cd6b6f4e5698357db26e
aakosm <aakos.mate@gmail.com>
2021-04-14 17:17:08 +0200

    first commit

---
+++ R/data_miniszterelnokok.R
@@ -0,0 +1,13 @@
+#' Miniszterelnöki beszédek minta
+#'
+#' Az adatbázis 7 darab véletlenszerűen kiválasztott miniszterelnöki beszédet tartalmaz a rendszerváltás utáni magyar miniszterelnököktől.
+#'
+#' @format Egy data.frame, 7 sorral, 4 oszloppal:
+#' \describe{
+#'    \item{doc_id}{A beszédek egyedi azonosítója}
+#'    \item{text}{A beszéd szövege}
+#'    \item{year}{A beszéd éve}
+#'    \item{pm}{A beszédet elmondó miniszterelnők neve}
+#' }
+#' @source \url{https://cap.tk.hu/en/dataoverview}
+"data_miniszterelnokok"
