ace0424e4f4183a1f8fa8dfdf78d4221ed262fa3
aakosm <aakos.mate@gmail.com>
2021-10-20 11:41:23 +0200

    updated with ady and petofi data

---
+++ R/data_ady.R
@@ -0,0 +1,11 @@
+#' Collected works of Endre Ady
+#'
+#' The data contains the collected works of the famous Hungarian poet, Endre Ady. It contains all of his works, without any cleaning or preprocessing.
+#'
+#' @format It is a `data.frame`, with 1 observation, 2 variables:
+#' \describe{
+#'    \item{doc_id}{Doc id variable for easier input into quanteda}
+#'    \item{text}{The unprocessed combined works of Endre Ady. All of the documents are contained as one observation (row).}
+#' }
+#'
+"data_ady"
---
+++ R/data_petofi.R
@@ -0,0 +1,11 @@
+#' Collected works of Sándor Petőfi
+#'
+#' The data contains the collected works of the famous Hungarian poet, Sándor Petőfi. It contains all of his works, without any cleaning or preprocessing.
+#'
+#' @format It is a `data.frame`, with 1 observation, 2 variables:
+#' \describe{
+#'    \item{doc_id}{Doc id variable for easier input into quanteda}
+#'    \item{text}{The unprocessed combined works of Sándor Petőfi. All of the documents are contained as one observation (row).}
+#' }
+#'
+"data_petofi"
