92da30aa1429a4916db3bb8c62b18596ce7a7f12
takacsanna <99684506+takacsanna@users.noreply.github.com>
2024-09-24 10:20:49 +0200

    add us_speeches data

---
+++ R/data_us_speeches.R
@@ -0,0 +1,12 @@
+#' US Speeches (May 2021)
+#'
+#' The dataset contains 45261 sentences from US speeches held in May 2021.
+#'
+#' @format It is a `data.frame`, with 45261 observation, 2 variables:
+#' \describe{
+#'    \item{text}{Sentence-segmented speech texts}
+#'    \item{label}{The CAP-label of the sentence}
+#' }
+#' @source \url{https://osf.io/ws5vp/}
+#'
+"data_us_speeches"
